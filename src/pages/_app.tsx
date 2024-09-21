c10e963842d0fe14699ca82ad9590adde6869af2
daimer003 <daimer003@gmail.com>
2024-09-20 21:08:26 -0500

    feat: config rute

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,22 @@
-import type { ReactElement, ReactNode } from 'react';
-import { useState, useEffect, useRef } from 'react';
-import '@/styles/globals.css';
-import { ThemeProvider } from 'styled-components';
-import { Background, theme } from '@/styles/Layaut/layaut';
+import type { ReactElement, ReactNode } from "react";
+import { useState, useEffect, useRef } from "react";
+import "@/styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { Background, theme } from "@/styles/Layaut/layaut";
 import "../styles/swiper.css";
 import { ComponentLoad } from "@/styles/Layaut/layaut";
-import { LogoSvg } from '@/components/smallComponents/logoSvg';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { LogoSvg } from "@/components/smallComponents/logoSvg";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import type { AppProps } from 'next/app';
-import createEmotionCache from 'src/createEmotionCache';
-import { NextPage } from 'next';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import Beer from './beer';
-import SeedInvestor from './investor';
+import type { AppProps } from "next/app";
+import createEmotionCache from "src/createEmotionCache";
+import { NextPage } from "next";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import Image from "next/image";
+// import { useRouter } from "next/router";
+// import Beer from "./beer";
+// import SeedInvestor from "./investor";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -33,58 +33,44 @@ export default function App(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   const getLayout = Component.getLayout ?? ((page) => page);
   const [dom, setDom] = useState(true);
-  const [subdomain, setSubdomain] = useState<string>("kryptopia");
+  // const [subdomain, setSubdomain] = useState<string>("kryptopia");
   const [errorVideo, setErrorVideo] = useState<boolean>(false);
   const videoRef = useRef(null);
-  const router = useRouter();
+  // const router = useRouter();
 
-  //* Obtiene el host y redirecciona a una ruta.
-  useEffect(() => {
-    const host = router?.query?.subdomain || window.location.host;
-    if (
-      host == "beer.kryptopia.io" ||
-      host == "https://beer.kryptopia.io" ||
-      host == "http://beer.kryptopia.io") {
-      setSubdomain("beer");
-    }
-    if (
-      host == "inversion.kryptopia.io" ||
-      host == "https://inversion.kryptopia.io" ||
-      host == "http://inversion.kryptopia.io"
-    ) {
-      setSubdomain("investor");
-    }
-  }, [router?.query?.subdomain])
-
-  //* filter domain
-  const SubDomain = () => {
-    switch (subdomain) {
-      case "beer":
-        return <Beer />
-        break;
-      case "investor":
-        return <SeedInvestor />
-        break;
-      case "kryptopia":
-        return <Component {...pageProps} />
-        break;
-    }
-  }
+  // //* Obtiene el host y redirecciona a una ruta.
+  // useEffect(() => {
+  //   const host = router?.query?.subdomain || window.location.host;
+  //   if (
+  //     host == "beer.kryptopia.io" ||
+  //     host == "https://beer.kryptopia.io" ||
+  //     host == "http://beer.kryptopia.io"
+  //   ) {
+  //     setSubdomain("beer");
+  //   }
+  //   if (
+  //     host == "inversion.kryptopia.io" ||
+  //     host == "https://inversion.kryptopia.io" ||
+  //     host == "http://inversion.kryptopia.io"
+  //   ) {
+  //     setSubdomain("investor");
+  //   }
+  // }, [router?.query?.subdomain]);
 
   useEffect(() => {
     setTimeout(() => {
       setDom(false);
-    }, 1000)
-  }, [dom])
+    }, 1000);
+  }, [dom]);
 
   useEffect(() => {
     const videoElement = videoRef.current;
     //* Ejecuta el video.
     const playVideo = () => {
       if (videoElement) {
-        videoElement.play().catch(error => {
+        videoElement.play().catch((error) => {
           setErrorVideo(true);
-          console.log("Error de video", error)
+          console.log("Error de video", error);
         });
       }
     };
@@ -95,44 +81,51 @@ export default function App(props: MyAppProps) {
 
   return (
     <CacheProvider value={emotionCache}>
-      {
-        getLayout(
-          <ThemeProvider theme={theme}>
-            <ToastContainer
-              position="top-right"
-              autoClose={8000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="dark" />
-            <Background>
-              {
-                errorVideo ?
-                  <Image src="/assets/img-background-body.webp" alt="Imagen background" width={500} height={900} priority />
-                  : <video
-                    ref={videoRef}
-                    controls={false}
-                    muted
-                    autoPlay
-                    loop
-                    poster='/assets/img-background-body.webp'>
-                    <source src='/assets/media/background.webm' type="video/webm" />
-                  </video>
-              }
-
-            </Background>
-            {dom ?
-              <ComponentLoad load={dom}>
-                <LogoSvg />
-              </ComponentLoad> :
-              <SubDomain />
-            }
-          </ThemeProvider>
-        )}
+      {getLayout(
+        <ThemeProvider theme={theme}>
+          <ToastContainer
+            position="top-right"
+            autoClose={8000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+          <Background>
+            {errorVideo ? (
+              <Image
+                src="/assets/img-background-body.webp"
+                alt="Imagen background"
+                width={500}
+                height={900}
+                priority
+              />
+            ) : (
+              <video
+                ref={videoRef}
+                controls={false}
+                muted
+                autoPlay
+                loop
+                poster="/assets/img-background-body.webp"
+              >
+                <source src="/assets/media/background.webm" type="video/webm" />
+              </video>
+            )}
+          </Background>
+          {dom ? (
+            <ComponentLoad load={dom}>
+              <LogoSvg />
+            </ComponentLoad>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </ThemeProvider>
+      )}
     </CacheProvider>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,33 @@
-import { useState } from 'react';
-import Navbar from '@/components/navbar/navbar';
-import Footer from '@/components/footer/footer';
-import Head from 'next/head';
-import HomeIndex from '@/components/sections/home';
-import Kryptoparner from './kryptopartner';
-import Commerce from './commerce';
-import { ContainerLayaut } from '@/styles/Layaut/layaut';
-import Step from './step';
-
+import { useState } from "react";
+import Navbar from "@/components/navbar/navbar";
+import Footer from "@/components/footer/footer";
+import Head from "next/head";
+import HomeIndex from "@/components/sections/home";
+import Kryptoparner from "./kryptopartner";
+import Commerce from "./commerce";
+import { ContainerLayaut } from "@/styles/Layaut/layaut";
+import Step from "./step";
 
 export default function Home() {
-  const [route, setRout] = useState('home');
-  const ruta = (r: string) => setRout(r)
+  const [route, setRout] = useState("home");
+  const ruta = (r: string) => setRout(r);
 
   //* Filter component
   const [routing, _] = useState({
-    home: < HomeIndex ruta={ruta} />,
+    home: <HomeIndex ruta={ruta} />,
     kryptoPartners: <Kryptoparner />,
     commerce: <Commerce />,
-    ayuda: <Step />
+    ayuda: <Step />,
   });
 
   return (
     <>
       <Head>
         <title>Kryptopia</title>
-        <meta name="description" content="¡Sé un pionero en el mercado y genera ingresos extras con toda la tecnología necesaria para facilitar los pagos crypto! Descubre el futuro del comercio en  línea con nuestra plataforma blockchain." />
+        <meta
+          name="description"
+          content="¡Sé un pionero en el mercado y genera ingresos extras con toda la tecnología necesaria para facilitar los pagos crypto! Descubre el futuro del comercio en  línea con nuestra plataforma blockchain."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -35,5 +37,5 @@ export default function Home() {
         <Footer />
       </ContainerLayaut>
     </>
-  )
+  );
 }
